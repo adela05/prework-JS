5eaa834ba6253a7a9badbba59598dbd3a2a82798
adela05 <aesparza051@gmail.com>
2019-06-12 15:14:26 -0500

    completed JavaScript Workbook

---
+++ workBook.js
@@ -0,0 +1,24 @@
+/* These are my answers from the JavaScript Workbook excerise. And I have completed the entire workbook but I will list a few exercise to this file. */
+
+//Types and Variables
+//Create a String containing your favorite food.(do not create a variable)
+console.log('Steak is my favorite');
+//Make an array with 3 string. The Strings can be anything you like.
+[ 'dog', 'cat', 'dragon' ];
+//Create a variable call favFood and assign it the value of your favorite food.
+var favFood = 'steak';
+//Write an expression to determine if 4 is less than 5 AND 4 is greater than 3
+4 < 5 && 4 > 3;
+// Using an object accessor (preferably dot notation), add code inside the parentheses of the console.log that will print sonic's species.
+let sonic = {
+	species: 'Hedgehog',
+	rings: 3000,
+	isInvincible: false,
+	health: 52
+};
+
+console.log(sonic.species);
+//Imagine that there is a variable called isLoggedIn that holds a boolean value. Write code that logs "Welcome" if the isLoggedIn is true.
+if (isLoggedIn === true) {
+	console.log('Welcome');
+}
